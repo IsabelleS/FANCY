496004234f40b326582896f3825905b16fbce555
IsabelleS <isabelle-sannier@hotmail.fr>
2016-02-24 10:50:21 +0100

    Added files via upload

---
+++ fancy.js
@@ -0,0 +1,24 @@
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 760) {
+        $('.menuScroll').fadeIn();
+    } else {
+        $('.menuScroll').fadeOut();
+    }
+
+    if (y < 940) {
+    	$('.scroll1').fadeIn();
+    }  else {
+    	$('.scroll1').fadeOut();
+    }
+
+    if (y > 940) {
+    	$('.scroll2').fadeIn();
+    } else {
+    	$('.scroll2').fadeOut();
+    }
+
+
+
+
+});
